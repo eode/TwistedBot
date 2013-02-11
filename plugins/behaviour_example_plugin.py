0775134b4f1fa770de8ad5176594d3388eee715d
Brian Visel <eode@eptitude.net>
2013-02-10 22:09:14 -0800

    slight cleanup

--- plugins/behaviour_example_plugin.py
+++ plugins/behaviour_example_plugin.py
@@ -6,6 +6,8 @@ log = logbot.getlogger("Example Plugin")
 
 
 def example(subject, context):
+    """Just logs a message when you say 'example' in chat."""
+    word, factory = context['world'], context['factory']
     log.msg('Example works! Received: ' + subject)
 
 
