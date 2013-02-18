0e1b8b4f2d0fa2c9072b2e0415b9fa0768c4c65f
Brian Visel <eode@eptitude.net>
2013-02-17 20:41:08 -0800

    Update plugin template

--- plugins/behaviour_example_plugin.py
+++ plugins/behaviour_example_plugin.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 from twistedbot.behaviours import BehaviourBase
 from twistedbot import logbot
@@ -5,9 +6,9 @@ from twistedbot import logbot
 log = logbot.getlogger("Example Plugin")
 
 
-def example(subject, context):
+def example(speaker, verb, data,  context):
     """Just logs a message when you say 'example' in chat."""
-    word, factory = context['world'], context['factory']
+    world, factory = context['world'], context['factory']
     log.msg('Example works! Received: ' + subject)
 
 
